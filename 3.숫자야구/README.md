70d3508a329238857b7a7f5a67113ce6e734d685
sunny-im <hellossun.dahae@gmail.com>
2023-05-11 23:36:47 +0900

    webpack

---
+++ 3.숫자야구/README.md
@@ -0,0 +1,6 @@
+### npm i vue
+### npm i webpack webpack-cli -D
+>> -D 옵션은, 개발할 때만 필요하므로 devDependencies에 넣기 위함
+
+### webpack.config.js
+>> 웹팩에 대한 설정을 두는 곳 (크게 4가지-entry, module, plugins, output)
---
+++ 3.숫자야구/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: {
+    app: './main.js', // 핵심파일
+  },
+  module: {
+    rules: [{
+      
+    }],
+  },
+  plugins: [],
+  output: {
+    filename: 'app.js', // 최종 결과물
+    path: './dist', // 폴더 경로
+  },
+};
