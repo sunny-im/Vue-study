2e0f8bf77ded577c042ce5928637aaeb7a3fe213
sunny-im <hellossun.dahae@gmail.com>
2023-05-17 23:02:45 +0900

    vue setting

--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,15 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+
 module.exports = {
+  mode: 'development',
+  devtool: 'eval',
+  // mode: 'production',
+  // devtool: 'hidden-source-map',
+  resolve: {
+    //확장자 처리 => import 할때 확장자 안넣어도 됨
+    extensions: ['.js',',vue']
+  },
   entry: {
     app: './main.js', // 핵심파일
   },
   module: {
     rules: [{
-      
+      // webpack은 기본적으로 javascript만 읽으므로 파일명이 .vue로 끝나는 파일도 읽을 수 있도록 정규식+vue파일을 읽을 수 있는 loader 설치 및 명시
+      test: /\.vue$/,
+      loader: 'vue-loader',
     }],
   },
-  plugins: [],
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
   output: {
     filename: 'app.js', // 최종 결과물
-    path: './dist', // 폴더 경로
+    // path: './dist', // 폴더 경로(절대경로))
+    // 현재경로는(__dirname) , 디렉토리 이름
+    path: path.join(__dirname, 'dist'),
   },
 };
